e395b912cc125036eaea89a9ae175689c2e363c7
FERARD Lucas <12305189@q10425.iutv.univ-paris13.fr>
2025-05-23 10:29:23 +0200

    ajout test addition

--- Exercice1.java
+++ Exercice1.java
@@ -1,7 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import Claculatrice.java;
 
 public class CalculatriceTest {
+
+    @BeforeEach
+    Calculatrice c = new Calculatrice
+
+    @Test
+    public void testAdd() {
+        assertEquals(8.0f, c.add(5.0f, 3.0f));
+    }
+
+
 
 
 
