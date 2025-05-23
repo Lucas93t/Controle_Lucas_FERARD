a4d24c38e27999f321bab595eb81bda5690399d8
FERARD Lucas <12305189@q10425.iutv.univ-paris13.fr>
2025-05-23 11:16:18 +0200

    ajout imports

--- StatistiqueTest.java
+++ StatistiqueTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
 public class StatistiqueTest {
 }
