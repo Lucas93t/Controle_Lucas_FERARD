59da4a8127a043e829676c5bcd13ada5570c9fdd
FERARD Lucas <12305189@q10425.iutv.univ-paris13.fr>
2025-05-23 11:52:46 +0200

    ajout exercice 3

--- CalculatriceTest.java
+++ CalculatriceTest.java
@@ -7,39 +7,39 @@ public class CalculatriceTest {
 
     @Test
     public void testAdd() {
-        Calculatrice c = new Calculatrice() ;
-        assertEquals(8.0f, c.add(5.0f, 3.0f));
+        Calculatrice clc = new Calculatrice() ;
+        assertEquals(8.0f, clc.add(5.0f, 3.0f));
     }
 
     @Test
     public void testDiv(){
-        Calculatrice c = new Calculatrice();
-        assertEquals(2.0f, c.div(4.0f, 2.0f));
+        Calculatrice clc = new Calculatrice();
+        assertEquals(2.0f, clc.div(4.0f, 2.0f));
     }
 
     @Test
 
     public void testMult(){
-            Calculatrice c = new Calculatrice();
-            assertEquals(10.0f, c.mult(5.0f, 2.0f));
+            Calculatrice clc = new Calculatrice();
+            assertEquals(10.0f, clc.mult(5.0f, 2.0f));
     }
 
     @Test
-    public void testDivByZero() {
-        Calculatrice c = new Calculatrice();
-        assertThrows(ArithmeticException.class, () -> c.div(4.0f, 0.0f));
+    public void testDivpParZero() {
+        Calculatrice clc = new Calculatrice();
+        assertThrows(ArithmeticException.class, () -> clc.div(4.0f, 0.0f));
     }
 
     @Test
     void testMinus() {
-        Calculatrice c = new Calculatrice();
-        assertEquals(4.0f, c.minus(7.0f, 3.0f));
+        Calculatrice clc = new Calculatrice();
+        assertEquals(4.0f, clc.minus(7.0f, 3.0f));
     }
 
     @Test
     void testMinusNegatif() {
-        Calculatrice c = new Calculatrice();
-        assertEquals(-2.0f, c.minus(3.0f, 5.0f));
+        Calculatrice clc = new Calculatrice();
+        assertEquals(-2.0f, clc.minus(3.0f, 5.0f));
 
     }
 
